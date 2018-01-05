049afec442cfb008c057f020274d4bfc9ab04236
Stephen <raj.stephen74@yahoo.com>
2018-01-05 17:11:49 +0530

    Added training project

---
+++ Training/Training/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Training
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Git is awesome - stesr");
+            Console.WriteLine("Git is awesome - stesr");
+            Console.ReadLine();
+        }
+    }
+}
